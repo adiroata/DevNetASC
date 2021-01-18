db0a874a410725f1cc463fa656370ecdeaf1d3be
Adrian Roata <adi.roata@gmail.com>
2021-01-18 18:14:57 +0200

    updated documentation

--- pyATS/install.sh
+++ pyATS/install.sh
@@ -7,12 +7,8 @@ source bin/activate
 # Get pyATS library
 pip install pyATS[library]
 
-
 # Get pyATS examples
 git clone https://github.com/CiscoTestAutomation/examples
 
 # Tests
 pyats run job examples/basic/basic_example_job.py
-
-
-
