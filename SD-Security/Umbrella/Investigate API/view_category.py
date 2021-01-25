0132f2bed53207d8e026f117ed7e269739cebca4
Adrian Roata <adi.roata@gmail.com>
2021-01-25 16:41:02 +0200

    updated Umbrella Investigate API code

--- SD-Security/Umbrella/Investigate API/view_category.py
+++ SD-Security/Umbrella/Investigate API/view_category.py
@@ -16,9 +16,26 @@ response = requests.request("GET", url, headers=headers, data=payload).json()
 
 # print(json.dumps(response, indent=2))
 
+print("-"*50)
+if response[f"{domain_name}"]["status"] == -1:
+    print("Site is malicious.")
+elif response[f"{domain_name}"]["status"] == 0:
+    print("Site is not categorized")
+elif response[f"{domain_name}"]["status"] == 1:
+    print("Site is ok.")
+
+print("-"*50)
 if response[f"{domain_name}"]["security_categories"] == []:
-    print("Domain name looks ok.")
+    print("Security threats list is empty.")
 else:
     print("Potential risks: ")
     for item in response[f"{domain_name}"]["security_categories"]:
-        print(item)
+        print("- " + item)
+
+print("-"*50)
+if response[f"{domain_name}"]["content_categories"] == []:
+    print("List is empty.")
+else:
+    print("Contents list: ")
+    for item in response[f"{domain_name}"]["content_categories"]:
+        print("- " + item)
