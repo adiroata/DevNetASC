f9798e7076cb7063b6a6df5e932fffc2cb629b55
Adrian Roata <adi.roata@gmail.com>
2021-01-25 15:14:09 +0200

    updated Umbrella Management API code

--- SD-Security/Umbrella/Management API/get_orgId.py
+++ SD-Security/Umbrella/Management API/get_orgId.py
@@ -9,7 +9,6 @@ headers = {
 api_key = os.environ.get("UMBRELLA_API_KEY")
 api_secret = os.environ.get("UMBRELLA_API_SECRET")
 
-# print(user, pw)
 
 response = requests.get(f"{url}", auth=(api_key, api_secret), headers = headers)
 
