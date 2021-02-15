7edf7e32715ee98265f92ec84f7c866174449810
Adrian Roata <adi.roata@gmail.com>
2021-02-15 17:20:15 +0200

    minor code changes

--- UCS/get_compute_objs.sh
+++ UCS/get_compute_objs.sh
@@ -1,7 +1,7 @@
 curl -k -X POST https://10.10.20.110/nuova \
 -H 'Content-Type: application/xml' \
--d '<configFindDnsByClassId
-    classId="computeItem"
-    cookie="1612193808/a2517098-53af-45c9-9679-3fb6569c20d3" />'\
+-d "<configFindDnsByClassId
+    classId='computeItem'
+    cookie=$UCS_COOKIE />"\
 | xmllint --pretty 2 -
 echo ''
